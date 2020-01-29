4eca5fd521d257fc304691191af22705b537b59a
Erik Muir <erikdmuir@gmail.com>
2020-01-29 12:12:37 -0600

    Update tfgen.sh

--- tfgen.sh
+++ tfgen.sh
@@ -6,14 +6,14 @@ terraform_name=$2
 
 # validate arguments
 validation_errors=""
-invalid_terraform_name='[^A-Za-z0-9_]'
+invalid_terraform_name_pattern='[^A-Za-z0-9_]'
 [[ -n "$bucket_name" ]] || validation_errors+="Bucket name is required. "
 [[ -n "$terraform_name" ]] || validation_errors+="Terraform name is required. "
-if [[ $terraform_name =~ $invalid_terraform_name ]] || [[ $terraform_name == _* ]]; then
+if [[ $terraform_name =~ $invalid_terraform_name_pattern ]] || [[ $terraform_name == _* ]]; then
   validation_errors+="Terraform name is invalid. "
 fi
 if [[ -n "$validation_errors" ]]; then
-  echo "$validation_errors" ; exit 1
+  echo "Error: $validation_errors" ; exit 1
 fi
 
 # get s3 buckets
@@ -28,7 +28,31 @@ for entry in $all_buckets; do
   fi
 done
 if [[ $bucket_exists -eq 0 ]]; then
-  echo "Could not find bucket '$bucket_name'" ; exit 1
+  echo "Error: Could not find bucket '$bucket_name'" ; exit 1
 fi
 
 echo "Found bucket '$bucket_name'"
+
+# config no other terraform resource with same name
+### config_exists=0
+### for filename in currentDirectory; do
+###   if [ $filename == *.tf ] && [ $filename.content contains $terraform_name ]; then
+###     config_exists=1
+###   fi
+### done
+### if [[ $config_exists -eq 1 ]]; then
+###   echo "Error: Terraform resource named '$terraform_name' already exists." ; exit 1
+### fi
+
+# config bucket not already managed
+### bucket_managed=0
+### for filename in currentDirectory; do
+###   if [ $filename == *.tf ] && [ $filename.content contains "bucket = $bucket_name" ]; then
+###     bucket_managed=1
+###   fi
+### done
+### if [[ $bucket_managed -eq 1 ]]; then
+###   echo "Error: Bucket '$bucket_name' is already managed by your Terraform config." ; exit 1
+### fi
+
+# create a new .tf file for the s3 bucket
